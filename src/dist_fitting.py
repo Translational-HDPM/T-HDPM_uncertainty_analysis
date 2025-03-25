51d714cbcc278aa45e18de540624d57d5a423b75
Debajyoti Debnath <debajyotid2@hotmail.com>
2025-03-25 17:57:38 -0400

    Add documentation to dist_fitting.py.

--- src/dist_fitting.py
+++ src/dist_fitting.py
@@ -9,17 +9,37 @@ import statsmodels.discrete.count_model as cm
 import statsmodels.discrete.discrete_model as smd
 
 
-def test_gamma_fit(data: Sequence[float]) -> tuple[float, float, float, float, float]:
+def test_gamma_fit(data: np.ndarray) -> tuple[float, float, float, float, float]:
     """
-    Performs  Kolmogorov-Smirnov (KS) Test on the fit of the gamma distribution on given data.
+    Performs Kolmogorov-Smirnov (KS) Test on the fit of the gamma distribution on given data.
+
+    Parameters
+    ----------
+    data
+        Numpy array of data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float, float]
+        KS Statistic, p-value and parameters from fitting the data to a gamma distribution.
     """
     fit_alpha, fit_loc, fit_beta = st.gamma.fit(data+1, floc=0)
     ks_stat, p_value = st.kstest(data+1, "gamma", args=(fit_alpha, fit_loc, fit_beta))
     return ks_stat, p_value, fit_alpha, fit_loc, fit_beta
 
-def test_lognormal_fit_ks(data: Sequence[float]) -> tuple[float, float, float, float, float]:
+def test_lognormal_fit_ks(data: np.ndarray) -> tuple[float, float, float, float, float]:
     """
-    Performs  Kolmogorov-Smirnov (KS) Test on the fit of the log normal distribution on given data.
+    Performs Kolmogorov-Smirnov (KS) Test on the fit of the log normal distribution on given data.
+
+    Parameters
+    ----------
+    data
+        Numpy array of data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float, float]
+        KS Statistic, p-value and parameters from fitting the data to a lognormal distribution.
     """
     if np.min(data) == 0:
         data += 1
@@ -27,9 +47,19 @@ def test_lognormal_fit_ks(data: Sequence[float]) -> tuple[float, float, float, f
     ks_stat, p_value = st.kstest(data, 'lognorm', args=(fit_shape, fit_loc, fit_scale))
     return ks_stat, p_value, fit_shape, fit_loc, fit_scale
 
-def test_lognormal_fit_sw(data: Sequence[float]) -> tuple[float, float]:
+def test_lognormal_fit_sw(data: np.ndarray) -> tuple[float, float]:
     """
     Performs Shapiro-Wilk Test on the fit of the log normal distribution on given data.
+
+    Parameters
+    ----------
+    data
+        Numpy array of data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float]
+        SW Statistic and p-value.
     """
     if np.min(data) == 0.0:
         data += 1
@@ -39,9 +69,19 @@ def test_lognormal_fit_sw(data: Sequence[float]) -> tuple[float, float]:
 
 def test_negative_binomial_fit(data: Sequence[float]) -> tuple[float, float, float, float, float]:
     """
-    Determines fit of a negative binomial distribution and returns the Akaike Information Criterion.
+    Determines fit of a negative binomial distribution against given data.
+
+    Parameters
+    ----------
+    data
+        Data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float, float]
+        AIC value, KS Statistic and p-value and parameters of the distribution fit.
     """
-    data = data.astype(np.int64)
+    data = np.asarray(data, dtype=np.int64)
     nb_model = smd.NegativeBinomial(data, np.ones_like(data)[:, np.newaxis])
     nb_results = nb_model.fit(disp=0)
     alpha, lambda_nb = nb_results.params["alpha"], np.exp(nb_results.params["const"])
@@ -56,8 +96,18 @@ def test_poisson_fit(data: Sequence[float]) -> tuple[float, float, float, float]
     """
     Determines fit of a Poisson distribution and returns the Akaike Information Criterion, 
     Kolmogorov-Smirnov (KS) test statistic, p-value and parameters of the fit distribution.
+
+    Parameters
+    ----------
+    data
+        Data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float]
+        AIC value, KS Statistic and p-value and parameters of the distribution fit.
     """
-    data = data.astype(np.int64)
+    data = np.asarray(data, dtype=np.int64)
     poisson_model = smd.Poisson(data, np.ones_like(data)[:, np.newaxis])
     poisson_results = poisson_model.fit(disp=0)
     lambda_p = np.exp(poisson_results.params["const"])
@@ -72,8 +122,18 @@ def test_zero_inflated_poisson_fit(data: Sequence[float]) \
     Determines fit of a zero-inflated Poisson distribution and returns the Akaike 
     Information Criterion, Kolmogorov-Smirnov (KS) test statistic, p-value and 
     parameters of the fit distribution.
+
+    Parameters
+    ----------
+    data
+        Data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float, float]
+        AIC value, KS Statistic and p-value and parameters of the distribution fit.
     """
-    data = data.astype(np.int64)
+    data = np.asarray(data, dtype=np.int64)
     zip_model = cm.ZeroInflatedPoisson(endog=data,
                                        exog=np.ones_like(data)[:, np.newaxis],
                                        exog_infl=np.ones_like(data)[:, np.newaxis],
@@ -98,8 +158,18 @@ def test_zero_inflated_negative_binomial_fit(data: Sequence[float]) ->\
     Determines fit of a zero-inflated negative binomial distribution and returns 
     the Akaike Information Criterion, Kolmogorov-Smirnov (KS) test statistic, 
     p-value and parameters of the fit distribution.
+
+    Parameters
+    ----------
+    data
+        Data to test fit against.
+
+    Returns
+    -------
+    tuple[float, float, float, float, float, float]
+        AIC value, KS Statistic and p-value and parameters of the distribution fit.
     """
-    data = data.astype(np.int64)
+    data = np.asarray(data, dtype=np.int64)
     zinb_model = cm.ZeroInflatedNegativeBinomialP(endog=data,
                           exog=np.ones_like(data)[:, np.newaxis],
                           exog_infl=np.ones_like(data)[:, np.newaxis],
